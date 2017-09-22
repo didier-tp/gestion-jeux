5154a847b42f660402296031698279bf94cb7fd3
jamal <bousfihajamal@hotmail.com>
2017-09-23 01:26:22 +0200

    résolution creation compte controller/ClientController.java

--- src/main/java/gestion/dao/CommandeDao.java
+++
@@ -1,7 +0,0 @@
-package gestion.dao;
-
-import gestion.model.Commande;
-
-public interface CommandeDao extends Dao<Commande, Long> {
-
-}
--- src/main/java/gestion/dao/LoginDao.java
+++
@@ -1,8 +0,0 @@
-package gestion.dao;
-
-import gestion.model.Login;
-
-public interface LoginDao extends Dao<Login, Long> {
-	public Login findByLogin(String login);
-
-}
